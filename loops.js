0e8da543ff834d5df93e07d3658814fab9f13ad9
Lorena <lorena.giddens@outlook.com>
2018-04-06 00:02:03 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,12 @@
+
+
+function forLoop(Array) {
+  for (let i = 0; i < 25; i++) {
+    if (i === 1) {
+      console.log("I am 1 strange loop.");
+    } else {
+      console.log(`I am ${i} strange loops.`);
+    }
+  }
+return Array;
+}
